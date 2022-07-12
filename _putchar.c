8580a3face137ee309b80136494e0c14c36fc506
Ovakoma <ideleovercomer18@gmail.com>
2022-07-12 11:40:12 -0700

    Collaborative work on printf function

--- _putchar.c
+++ _putchar.c
@@ -2,11 +2,8 @@
 
 /**
  * _putchar - Entry point
- *
  * Description: Writes the character c to stdout
- *
  * @c: The character to print
- * 
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
--- base_changer.c
+++ base_changer.c
@@ -17,7 +17,7 @@ int print_binary(va_list list)
 	num = va_arg(list, unsigned int);
 	if (num == 0)
 		return (_putchar('0'));
-	if (num < 1 )
+	if (num < 1)
 		return (-1);
 	len = base_len(num, 2);
 	str = malloc(sizeof(char) * len + 1);
